1b18c59a722b4555532ba755568062d8ab1c998e
Brian Devins <brian.devins@coxautoinc.com>
2017-09-15 10:55:51 -0400

    Fix the nginx file generation

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-envsubst < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst '$BACKEND_URL $AUTH_SERVER_URL $PROXY_URL_BASE' < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
