e740b161b8dba272a326e4412f55c1a04acda77b
Brian Devins <brian.devins@coxautoinc.com>
2017-09-15 10:58:39 -0400

    Don't cat the config

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 envsubst '$BACKEND_URL $AUTH_SERVER_URL $PROXY_URL_BASE' < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
